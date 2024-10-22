17fbd450266c90bd1f1ae0296042b4a7893ee33e
Deepak07S25 <deepakshy94@gmail.com>
2024-10-22 11:33:21 +0530

    Frontend Basic Structure

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes , Route} from "react-router-dom";
+import Body from "./Body";
+import Profile from "./Profile";
+import Login from "./Login";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter basename="/">
+       <Routes>
+        <Route path="/" element={< Body/>}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+       </Routes >
+      </BrowserRouter>
+      
+    </>
+  );
+}
+
+export default App;
