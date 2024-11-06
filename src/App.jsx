8d8ba2010697207ce6b15200ccbb2a8670b3cb56
Deepak07S25 <deepakshy94@gmail.com>
2024-11-06 12:35:09 +0530

    connections

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Body from "./components/Body";
-import Profile from "./components/Profile";
 import Login from "./components/Login";
-import { Provider } from "react-redux"; // Correct Provider import
-import appStore from "./utils/appStore"; // Redux store
+import Profile from "./components/Profile";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 import Feed from "./components/Feed";
-
+import Connections from "./components/Connections";
+import Requests from "./components/Requests";
 
 function App() {
   return (
-    <Provider store={appStore}> {/* Correctly passing the store */}
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/" element={<Feed />} /> {/* Relative paths */}
-            <Route path="login" element={<Login />} /> {/* Relative paths */}
-            <Route path="profile" element={<Profile />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/connections" element={<Connections />} />
+              <Route path="/requests" element={<Requests />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </>
   );
 }
 
